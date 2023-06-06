24311c454a18c33090c0aa843862540af3a79eac
mohit49 <mohitanim@gmail.com>
2023-06-07 00:47:06 +0530

    change

--- src/includes/Header/Header.js
+++ src/includes/Header/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
 <HeaderElements>
   <ColoumnCenter>
-  <h5>TuesDay, 28 April 23</h5>
+  <h5 title={new Date().toJSON().slice(0,10).replace(/-/g,'/')}>{new Date().toJSON().slice(0,10).replace(/-/g,'/')}</h5>
   <p>Delhi, Indi</p>
   </ColoumnCenter>
 </HeaderElements>
